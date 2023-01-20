586b1a8b3ce352480cf3280955005f0d13d1eb9c
Sankalp <sankalp.kulkarni05@gmail.com>
2023-01-21 00:47:26 +0530

    [Sankalp]- Welcome msg

---
+++ BinarySearchTreeDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BinarySearchTreeDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree Program");
+        }
+    }
+}
